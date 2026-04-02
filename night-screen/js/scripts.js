25c3593e6e19c286233fb71de00caaed7ec194b3
Dizzy <spiderllama@gmail.com>
2026-04-02 18:20:05 -0500

    added random location, opacity, and scale to eyes, and changed frequency

--- night-screen/js/scripts.js
+++ night-screen/js/scripts.js
@@ -12,7 +12,7 @@ $("#close-fullscreen-btn").click(function () {
 });
 
 /*This insures that the close function still runs when pressing escape.*/
-document.addEventListener("fullscreenchange", function() {
+document.addEventListener("fullscreenchange", function () {
   isFullscreen = !isFullscreen;
   if (isFullscreen == false) {
     browserClass();
@@ -27,11 +27,14 @@ var elem = document.documentElement;
 function openFullscreen() {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
     elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
     elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
     elem.msRequestFullscreen();
   }
 }
@@ -40,11 +43,14 @@ function openFullscreen() {
 function closeFullscreen() {
   if (document.exitFullscreen) {
     document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) { /* Firefox */
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
     document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
     document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE/Edge */
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
     document.msExitFullscreen();
   }
 }
@@ -59,28 +65,67 @@ function browserClass() {
 }
 
 /***Random Eyes***/
+
+var toggleEyes = "on";
+
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-
+function initEyes() {
+  if (toggleEyes == "on") {
+    let xVect = getRandomInt(3, 86);
+    let yVect = getRandomInt(5, 91);
+    let scale = getRandomInt(25, 126);
+    let opacity = getRandomInt(15, 81);
+    $("#eye-left").css("left", xVect + "vw");
+    $("#eye-left").css("top", yVect + "vh");
+    $("#eye-right").css("left", "calc(" + xVect + "vw + " + 125 * (scale / 100) + "px)");
+    $("#eye-right").css("top", yVect + "vh");
+    $(".eyes").css("scale", scale + "%");
+    $(".eyes").css("opacity", opacity + "%");
+    console.log(
+      "Eyes reset - position: " + xVect + "vw, " + yVect + "vh | scale: " + scale + "% | opacity: " + opacity + "%"
+    );
+    openEyes();
+  } else {
+    console.log("Eyes turned off.");
+  }
+}
 
 function openEyes() {
-  $('.eyes').removeClass('closed');
-  $('.eyes').addClass('open');
-  let randomTime = getRandomInt(10000, 15000);
-  console.log('Eyes are open. closeEyes() will execute in ' + (randomTime/1000) + ' seconds.');
-  setTimeout(closeEyes, randomTime);
+  $(".eyes").removeClass("closed");
+  $(".eyes").addClass("open");
+  let randomDelay = getRandomInt(3000, 15001);
+  console.log("Eyes are open. closeEyes() will execute in " + randomDelay / 1000 + " seconds.");
+  setTimeout(closeEyes, randomDelay);
 }
 function closeEyes() {
-  $('.eyes').removeClass('open');
-  $('.eyes').addClass('closed');
-  let randomTime = getRandomInt(10000, 15000);
-  console.log('Eyes are closed. openEyes() will execute in ' + (randomTime/1000) + ' seconds.');
-  setTimeout(openEyes, randomTime);
+  $(".eyes").removeClass("open");
+  $(".eyes").addClass("closed");
+  let randomDelay = getRandomInt(5000, 30001);
+  console.log("Eyes are closed. Eyes will reset in " + randomDelay / 1000 + " seconds.");
+  setTimeout(initEyes, randomDelay);
 }
-let initialRandomTime = getRandomInt(10000, 15000);
-setTimeout(openEyes, initialRandomTime);
-console.log("openEyes() will execute in " + (initialRandomTime/1000) + " seconds.");
+
+initEyes();
+
+$("#eyes-on-btn").click(function () {
+  $(".eyes").removeClass("open");
+  $(".eyes").addClass("closed");
+  $("#eyes-on-btn").removeClass("active");
+  $("#eyes-off-btn").addClass("active");
+  console.log("Turning off eyes.");
+  toggleEyes = "off";
+});
+$("#eyes-off-btn").click(function () {
+  $(".eyes").removeClass("closed");
+  $(".eyes").addClass("open");
+  $("#eyes-off-btn").removeClass("active");
+  $("#eyes-on-btn").addClass("active");
+  console.log("Turning on eyes.");
+  toggleEyes = "on";
+  initEyes();
+});

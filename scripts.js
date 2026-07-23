ca8673b5aec520cc132e454b528b479155b7efc9
Dizzy <spiderllama@gmail.com>
2026-07-23 15:05:24 -0500

    Finished responsive styling for mobile and got menu button working

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function init() {
+  const menu = document.querySelector(".full-menu");
+}
+
+function toggleMenu() {
+  const menu = document.querySelector(".full-menu");
+  if (menu.classList.contains("closed")) {
+    console.log("opened");
+    menu.classList.remove("closed");
+    menu.classList.add("open");
+    return;
+  } else if (menu.classList.contains("open")) {
+    console.log("closed");
+    menu.classList.remove("open");
+    menu.classList.add("closed");
+    return;
+  }
+}
+
+document.querySelector("#menu-btn").addEventListener("click", toggleMenu);
+
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+});
